12bf69ae5bf8fad7a265bf59ea402ecc4c511403
Mike Botsko <botsko@gmail.com>
2012-06-14 20:02:16 -0700

    more work

--- src/me/botsko/dhmcores/DhmcOres.java
+++ src/me/botsko/dhmcores/DhmcOres.java
@@ -1,10 +1,13 @@
 package me.botsko.dhmcores;
 
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
 import me.botsko.dhmcores.listeners.OresBlockBreakEvent;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,6 +15,13 @@ public class DhmcOres extends JavaPlugin {
 
 	protected Logger log = Logger.getLogger("Minecraft");
 	protected FileConfiguration config;
+	
+	
+	/**
+	 * Store locations of blocks already alerted so we don't count
+	 * them again. This is only temporary.
+	 */
+	public ConcurrentHashMap<Location,Long> alertedBlocks = new ConcurrentHashMap<Location,Long>();
 	
 	
     /**
@@ -22,9 +32,32 @@ public class DhmcOres extends JavaPlugin {
 		
 		this.log("Initializing plugin.");
 		
+		removeExpiredLocations();
+		
 		// Assign event listeners
 		getServer().getPluginManager().registerEvents(new OresBlockBreakEvent( this ), this);
 		
+	}
+	
+	
+	/**
+	 * If a user disconnects in an unknown way that is never caught by onPlayerQuit,
+	 * this will force close all records except for players currently online.
+	 */
+	public void removeExpiredLocations(){
+		getServer().getScheduler().scheduleAsyncRepeatingTask(this, new Runnable() {
+
+		    public void run() {
+		    	java.util.Date date = new java.util.Date();
+		    	// Remove locations logged over five minute ago.
+		    	for (Entry<Location, Long> entry : alertedBlocks.entrySet()){
+		    		long diff = (date.getTime() - entry.getValue()) / 1000;
+		    		if(diff >= 30){
+		    			alertedBlocks.remove(entry.getKey());
+		    		}
+		    	}
+		    }
+		}, 1200L, 1200L);	
 	}
 	
 	
@@ -35,7 +68,7 @@ public class DhmcOres extends JavaPlugin {
 	 */
 	public String playerMsg(String msg){
 		if(msg != null){
-			return ChatColor.GOLD + "[ORE]: " + ChatColor.WHITE + msg;
+			return ChatColor.RED + "[ORE]: " + ChatColor.WHITE + msg;
 		}
 		return "";
 	}
@@ -48,7 +81,7 @@ public class DhmcOres extends JavaPlugin {
 	 */
 	public String playerError(String msg){
 		if(msg != null){
-			return ChatColor.GOLD + "[ORE]: " + ChatColor.RED + msg;
+			return ChatColor.RED + "[ORE]: " + ChatColor.RED + msg;
 		}
 		return "";
 	}
--- src/me/botsko/dhmcores/listeners/OresBlockBreakEvent.java
+++ src/me/botsko/dhmcores/listeners/OresBlockBreakEvent.java
@@ -3,7 +3,7 @@ package me.botsko.dhmcores.listeners;
 import java.util.ArrayList;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -17,13 +17,16 @@ import me.botsko.dhmcores.DhmcOres;
 
 public class OresBlockBreakEvent implements Listener {
 	
+	/**
+	 * 
+	 */
 	private DhmcOres plugin;
+
 	
 	/**
-	 * Store locations of blocks already alerted so we don't count
-	 * them again. This is only temporary.
+	 * It's the date yo!
 	 */
-	protected ArrayList<Location> alertedBlocks = new ArrayList<Location>();
+	protected java.util.Date date = new java.util.Date();
 	
 	
 	/**
@@ -47,14 +50,18 @@ public class OresBlockBreakEvent implements Listener {
 		
 //		System.out.print("Block Location: " + block.getLocation().toString());
 //		System.out.print("Alerted Has Location: " + alertedBlocks.contains( block.getLocation() ));
-		
-		if(block != null && isWatched(block) && !alertedBlocks.contains( block.getLocation() )){
-			// identify all ore blocks on same Y axis in x/z direction
-			ArrayList<Block> matchingBlocks = new ArrayList<Block>();
-			ArrayList<Block> foundores = findNeighborBlocks( block.getType(), block, matchingBlocks );
-			if(!foundores.isEmpty()){
-				for (Player p : player.getServer().getOnlinePlayers()) {
-					p.sendMessage( plugin.playerMsg( player.getName() + " found " + foundores.size() + " " + getOreColor(block) + getOreNiceName(block) ) );
+		if(player.getGameMode() != GameMode.CREATIVE){
+			if(block != null && isWatched(block) && !plugin.alertedBlocks.containsKey( block.getLocation() )){
+				// identify all ore blocks on same Y axis in x/z direction
+				ArrayList<Block> matchingBlocks = new ArrayList<Block>();
+				ArrayList<Block> foundores = findNeighborBlocks( block.getType(), block, matchingBlocks );
+				if(!foundores.isEmpty()){
+					for (Player p : player.getServer().getOnlinePlayers()) {
+						if (p.hasPermission("dhmcores.alert")){
+//							int light = Math.round(((block.getLightLevel()) & 0xFF) * 100) / 15;
+							p.sendMessage( plugin.playerMsg( getOreColor(block) + player.getName() + " found " + foundores.size() + " " + getOreNiceName(block) ) );
+						}
+					}
 				}
 			}
 		}
@@ -115,7 +122,9 @@ public class OresBlockBreakEvent implements Listener {
         if(isWatched(currBlock)){
         	
         	matchingBlocks.add(currBlock);
-        	alertedBlocks.add(currBlock.getLocation());
+        	
+        	plugin.alertedBlocks.put(currBlock.getLocation(), date.getTime());
+        	
 //        	System.out.print("Found Block Location: " + currBlock.getLocation().toString());
         	
         	Block xPositive = currBlock.getRelative(1, 0, 0);
@@ -125,21 +134,27 @@ public class OresBlockBreakEvent implements Listener {
             Block yPositive = currBlock.getRelative(0, 1, 0);
             Block yNegative = currBlock.getRelative(0, -1, 0);
 
+            // n
         	if ( !matchingBlocks.contains(xPositive) ) {
         		findNeighborBlocks( type, xPositive, matchingBlocks );
         	}
+        	// s
         	if ( !matchingBlocks.contains(xNegative) ) {
         		findNeighborBlocks( type, xNegative, matchingBlocks );
         	}
+        	// e
         	if ( !matchingBlocks.contains(zPositive) ) {
         		findNeighborBlocks( type, zPositive, matchingBlocks );
         	}
+        	// w
         	if ( !matchingBlocks.contains(zNegative) ) {
         		findNeighborBlocks( type, zNegative, matchingBlocks );
         	}
+        	// u
         	if ( !matchingBlocks.contains(yPositive) ) {
         		findNeighborBlocks( type, yPositive, matchingBlocks );
         	}
+        	// d
         	if ( !matchingBlocks.contains(yNegative) ) {
         		findNeighborBlocks( type, yNegative, matchingBlocks );
         	}
